2fc500499b81cf0303574af11026c7d30ffb8fa1
rohmanhida <rohmanhida@rohmanhidas-MacBook-Pro.local>
2025-12-28 11:06:26 +0700

    wip

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) { }
+
+  async validateUser(username: string, pass: string): Promise<any> {
+    const user = await this.usersService.findOne(username);
+    if (user && user.password === pass) {
+      const { password, ...result } = user;
+      return result
+    }
+  }
+}
